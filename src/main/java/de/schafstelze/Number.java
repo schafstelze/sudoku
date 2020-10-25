bf2ab2d47e3f0f1edbe61aa0e72d5a5ebda27372
schafstelze <a.thierbach@autohaus24.de>
2020-10-25 17:30:56 +0100

    Add string representation of sudoku

--- src/main/java/de/schafstelze/Number.java
+++ src/main/java/de/schafstelze/Number.java
@@ -4,7 +4,25 @@ import java.util.List;
 
 public enum Number {
 
-	ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE;
-	
+	ONE("1"),
+	TWO("2"),
+	THREE("3"),
+	FOUR("4"),
+	FIVE("5"),
+	SIX("6"),
+	SEVEN("7"),
+	EIGHT("8"),
+	NINE("9");
+
+	private final String integerValue;
+
+	Number(String integerValue) {
+		this.integerValue = integerValue;
+	}
+
+	public String asInteger() {
+		return this.integerValue;
+	}
+
 	public static final List<Number> NUMBER_LIST = Arrays.asList(Number.values());
 }
--- src/main/java/de/schafstelze/Sudoku.java
+++ src/main/java/de/schafstelze/Sudoku.java
@@ -3,6 +3,7 @@ package de.schafstelze;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import com.google.common.collect.ArrayTable;
 import com.google.common.collect.Table;
@@ -10,6 +11,30 @@ import com.google.common.collect.Table;
 import static de.schafstelze.Number.NUMBER_LIST;
 
 public class Sudoku {
+
+	private static final String FIELD_AS_STRING =
+            									"""
+												=========================================
+												||1-1|1-2|1-3||1-4|1-5|1-6||1-7|1-8|1-9||
+												-----------------------------------------
+												||2-1|2-2|2-3||2-4|2-5|2-6||2-7|2-8|2-9||
+												-----------------------------------------
+												||3-1|3-2|3-3||3-4|3-5|3-6||3-7|3-8|3-9||
+												=========================================
+												||4-1|4-2|4-3||4-4|4-5|4-6||4-7|4-8|4-9||
+												-----------------------------------------
+												||5-1|5-2|5-3||5-4|5-5|5-6||5-7|5-8|5-9||
+												-----------------------------------------
+												||6-1|6-2|6-3||6-4|6-5|6-6||6-7|6-8|6-9||
+												=========================================
+												||7-1|7-2|7-3||7-4|7-5|7-6||7-7|7-8|7-9||
+												-----------------------------------------
+												||8-1|8-2|8-3||8-4|8-5|8-6||8-7|8-8|8-9||
+												-----------------------------------------
+												||9-1|9-2|9-3||9-4|9-5|9-6||9-7|9-8|9-9||
+												=========================================
+												""";
+
 
 	private final Table<Number, Number, Cell> field;
 	
@@ -37,6 +62,23 @@ public class Sudoku {
 		List<Cell> cells = new ArrayList<>();
 		NUMBER_LIST.stream().forEach(number -> cells.add(input.get(number)));
 		return cells;
+	}
+
+	@Override
+	public String toString() {
+		String output = FIELD_AS_STRING;
+
+		for (Number rowNumber: NUMBER_LIST) {
+			for (Number cellNumber: NUMBER_LIST) {
+				Cell cell = field.get(rowNumber, cellNumber);
+				String cellIdentifier = rowNumber.asInteger() + "-" + cellNumber.asInteger();
+				String cellValue = cell.getContent() == null ? "   " : " " + cell.getContent().asInteger() + " ";
+
+				output = output.replace(cellIdentifier, cellValue);
+			}
+		}
+
+		return output;
 	}
 	
 	public static class SudokuBuilder {
